c56b88ae566c5f5ecccc81e07a97ac9db45917f4
Jan-Eric Fernlöf <jef@clevercode.se>
2021-04-05 13:02:32 +0200

    Rewrite to always return true from PlayerController.FixedUpdate prefix-patch Bump version to 0.4.

--- RagnarsRokare_DodgeOnDoubleTap/DodgeOnDoubleTap.cs
+++ RagnarsRokare_DodgeOnDoubleTap/DodgeOnDoubleTap.cs
@@ -6,34 +6,39 @@ using UnityEngine;
 
 namespace RagnarsRokare_DodgeOnDoubleTap
 {
-	[BepInPlugin("RagnarsRokare.DodgeOnDoubleTap", "RagnarsRökare DodgeOnDoubleTapMod", "0.3")]
+	[BepInPlugin(ModId, ModName, ModVersion)]
 	[BepInProcess("valheim.exe")]
     public class DodgeOnDoubleTap : BaseUnityPlugin
     {
-        private readonly Harmony harmony = new Harmony("RagnarsRokare.DodgeOnDoubleTap");
+		public const string ModId = "RagnarsRokare.DodgeOnDoubleTap";
+		public const string ModName = "RagnarsRökare DodgeOnDoubleTapMod";
+		public const string ModVersion = "0.4";
+
+		private readonly Harmony harmony = new Harmony(ModId);
 		public static ConfigEntry<int> DodgeTapHoldMax;
 		public static ConfigEntry<int> DodgeDoubleTapDelay;
 		public static ConfigEntry<int> NexusID;
 
 		void Awake()
         {
-            harmony.PatchAll();
+			Debug.Log($"Loading {ModName} v{ModVersion}, lets get rolling!");
+			harmony.PatchAll();
 			DodgeTapHoldMax = Config.Bind("General", "DodgeTapHoldMax", 200);
 			DodgeDoubleTapDelay = Config.Bind("General", "DodgeDoubleTapDelay", 300);
 			NexusID = Config.Bind<int>("General", "NexusID", 871, "Nexus mod ID for updates");
 		}
 
-		public enum DodgeDirection { None, Forward, Backwards, Left, Right };
+		public enum DodgeDirection { None, Forward, Backward, Left, Right };
 		public static DodgeDirection DodgeDir { get; set; } = DodgeDirection.None;
 
 		[HarmonyPatch(typeof(Player), "SetControls")]
 		class SetControls_Patch
 		{
-			static bool Prefix(ref float ___m_queuedDodgeTimer, ref Vector3 ___m_queuedDodgeDir, Vector3 ___m_lookDir)
+			static void Postfix(ref float ___m_queuedDodgeTimer, ref Vector3 ___m_queuedDodgeDir, Vector3 ___m_lookDir)
             {
 				if (DodgeDir == DodgeDirection.None)
                 {
-					return true;
+					return ;
                 }
 
 				___m_queuedDodgeTimer = 0.5f;
@@ -42,13 +47,12 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 				dodgeDir.y = 0f;
 				dodgeDir.Normalize();
 
-				if (DodgeDir == DodgeDirection.Backwards)  dodgeDir = -dodgeDir;
+				if (DodgeDir == DodgeDirection.Backward)  dodgeDir = -dodgeDir;
 				else if (DodgeDir == DodgeDirection.Left) dodgeDir = Quaternion.AngleAxis(-90, Vector3.up) * dodgeDir;
 				else if (DodgeDir == DodgeDirection.Right) dodgeDir = Quaternion.AngleAxis(90, Vector3.up) * dodgeDir;
 
 				___m_queuedDodgeDir = dodgeDir;
 				DodgeDir = DodgeDirection.None;
-				return false;
 			}
 		}
 
@@ -70,25 +74,24 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 			private static float m_leftPressTimer = 0;
 			private static float m_rightPressTimer = 0;
 
-			static bool Prefix(ZNetView ___m_nview, ref Player ___m_character, ref bool ___m_attackWasPressed, ref bool ___m_secondAttackWasPressed, ref bool ___m_blockWasPressed, ref bool ___m_lastJump, ref bool ___m_lastCrouch)
+			static bool Prefix(PlayerController __instance, ZNetView ___m_nview)
             {
 				if ((bool)___m_nview && !___m_nview.IsOwner())
 				{
-					return false;
+					return true;
 				}
-				if (!TakeInput())
+				if (!(bool)Traverse.Create(__instance).Method("TakeInput").GetValue())
 				{
-					___m_character.SetControls(Vector3.zero, attack: false, attackHold: false, secondaryAttack: false, block: false, blockHold: false, jump: false, crouch: false, run: false, autoRun: false);
-					return false;
+					return true;
 				}
-				bool flag = InInventoryEtc();
-				Vector3 zero = Vector3.zero;
-				bool run = ZInput.GetButton("Run") || ZInput.GetButton("JoyRun");
+				if ((bool)Traverse.Create(__instance).Method("InInventoryEtc").GetValue())
+                {
+					return true;
+                }
 				if (ZInput.GetButton("Forward"))
 				{
 					DetectTap(true, (float)(DateTime.Now - m_forwardLastTapCheck).TotalMilliseconds, DodgeTapHoldMax.Value, ref m_forwardPressTimer);
 					m_forwardLastTapCheck = DateTime.Now;
-					zero.z += 1f;
 				}
 				else
                 {
@@ -100,19 +103,17 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 				{
 					DetectTap(true, (float)(DateTime.Now - m_backwardLastTapCheck).TotalMilliseconds, DodgeTapHoldMax.Value, ref m_backwardPressTimer);
 					m_backwardLastTapCheck = DateTime.Now;
-					zero.z -= 1f;
 				}
 				else
 				{
 					bool isTap = DetectTap(false, (float)(DateTime.Now - m_backwardLastTapCheck).TotalMilliseconds, DodgeTapHoldMax.Value, ref m_backwardPressTimer);
 					m_backwardLastTapCheck = DateTime.Now;
-					CheckForDoubleTapDodge(isTap, ref m_backwardLastTapRegistered, DodgeDirection.Backwards);
+					CheckForDoubleTapDodge(isTap, ref m_backwardLastTapRegistered, DodgeDirection.Backward);
 				}
 				if (ZInput.GetButton("Left"))
 				{
 					DetectTap(true, (float)(DateTime.Now - m_leftLastTapCheck).TotalMilliseconds, DodgeTapHoldMax.Value, ref m_leftPressTimer);
 					m_leftLastTapCheck = DateTime.Now;
-					zero.x -= 1f;
 				}
 				else
 				{
@@ -124,7 +125,6 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 				{
 					DetectTap(true, (float)(DateTime.Now - m_rightLastTapCheck).TotalMilliseconds, DodgeTapHoldMax.Value, ref m_rightPressTimer);
 					m_rightLastTapCheck = DateTime.Now;
-					zero.x += 1f;
 				}
 				else
 				{
@@ -132,34 +132,7 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 					m_rightLastTapCheck = DateTime.Now;
 					CheckForDoubleTapDodge(isTap, ref m_rightLastTapRegistered, DodgeDirection.Right);
 				}
-				zero.x += ZInput.GetJoyLeftStickX();
-				zero.z += 0f - ZInput.GetJoyLeftStickY();
-				if (zero.magnitude > 1f)
-				{
-					zero.Normalize();
-				}
-				bool flag2 = (ZInput.GetButton("Attack") || ZInput.GetButton("JoyAttack")) && !flag;
-				bool attackHold = flag2;
-				bool attack = flag2 && !___m_attackWasPressed;
-				___m_attackWasPressed = flag2;
-				bool flag3 = (ZInput.GetButton("SecondAttack") || ZInput.GetButton("JoySecondAttack")) && !flag;
-				bool secondaryAttack = flag3 && !___m_secondAttackWasPressed;
-				___m_secondAttackWasPressed = flag3;
-				bool flag4 = (ZInput.GetButton("Block") || ZInput.GetButton("JoyBlock")) && !flag;
-				bool blockHold = flag4;
-				bool block = flag4 && !___m_blockWasPressed;
-				___m_blockWasPressed = flag4;
-				bool button = ZInput.GetButton("Jump");
-				bool jump = (button && !___m_lastJump) || ZInput.GetButtonDown("JoyJump");
-				___m_lastJump = button;
-				bool flag5 = InventoryGui.IsVisible();
-				bool flag6 = (ZInput.GetButton("Crouch") || ZInput.GetButton("JoyCrouch")) && !flag5;
-				bool crouch = flag6 && !___m_lastCrouch;
-				___m_lastCrouch = flag6;
-				bool button2 = ZInput.GetButton("AutoRun");
-				___m_character.SetControls(zero, attack, attackHold, secondaryAttack, block, blockHold, jump, crouch, run, button2);
-
-				return false;
+				return true;
 			}
 
             private static void CheckForDoubleTapDodge(bool isTap, ref DateTime? lastTapRegistered, DodgeDirection dodgeDirection)
@@ -169,7 +142,6 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 					var milliesSinceLastTap = (DateTime.Now - lastTapRegistered)?.TotalMilliseconds ?? DodgeDoubleTapDelay.Value;
 					if (milliesSinceLastTap < DodgeDoubleTapDelay.Value)
 					{
-						//Debug.Log($"DOUBLETAP! => {milliesSinceLastTap}");
 						DodgeDir = dodgeDirection;
 					}
 					else
@@ -180,7 +152,6 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 					if (lastTapRegistered == null)
                     {
 						lastTapRegistered = DateTime.Now;
-						//Debug.Log($"TAP! => {lastTapRegistered.Value.ToString("mm:ss:FFF")}");
 					}
                 }
             }
@@ -190,44 +161,16 @@ namespace RagnarsRokare_DodgeOnDoubleTap
 				if (isPressed)
                 {
 					pressTimer += timeSinceLastCheck;
-					//Debug.Log("PRESS");
 					return false;
                 }
                 else if (pressTimer > 0)
 				{
 					bool isTap = pressTimer < maxPressTime;
 					pressTimer = 0;
-					//Debug.Log("RELEASE");
 					return isTap;
 				}
 				return false;
             }
-
-			private static bool TakeInput()
-			{
-				if (GameCamera.InFreeFly())
-				{
-					return false;
-				}
-				if ((!Chat.instance || !Chat.instance.HasFocus()) && !Menu.IsVisible() && !Console.IsVisible() && !TextInput.IsVisible() && !Minimap.InTextInput() && (!ZInput.IsGamepadActive() || !Minimap.IsOpen()) && (!ZInput.IsGamepadActive() || !InventoryGui.IsVisible()) && (!ZInput.IsGamepadActive() || !StoreGui.IsVisible()))
-				{
-					if (ZInput.IsGamepadActive())
-					{
-						return !Hud.IsPieceSelectionVisible();
-					}
-					return true;
-				}
-				return false;
-			}
-
-			private static bool InInventoryEtc()
-			{
-				if (!InventoryGui.IsVisible() && !Minimap.IsOpen() && !StoreGui.IsVisible())
-				{
-					return Hud.IsPieceSelectionVisible();
-				}
-				return true;
-			}
 		}
 	}
 }
