9e0bdd0e9c54ee7abaa1dc9745ac78bef894e7c9
MrTorstensson <30778800+MrTorstensson@users.noreply.github.com>
2021-04-26 21:56:56 +0200

    Assignment Patch

---
+++ SlaveGreylings/Patches/Assignments_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using RagnarsRokare.MobAI;
+
+namespace SlaveGreylings
+{
+    public partial class SlaveGreylings
+    {
+        [HarmonyPatch(typeof(Switch), "Interact")]
+        static class Switch_Interact_Patch
+        {
+            public static void Postfix(Switch __instance)
+            {
+                foreach (MobAIBase mob in MobManager.Mobs)
+                {
+
+                }
+
+                string name = Common.GetPrefabName(__instance.gameObject.name);
+            }
+        }
+    }
+}
