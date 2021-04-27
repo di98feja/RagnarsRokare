fbc3a442afd711deafad0f6a2725cf193d60ceb5
Jan-Eric Fernlöf <jef@clevercode.se>
2021-04-27 18:25:54 +0200

    Copied base configs

--- SlaveGreylings/BruteAI.cs
+++ SlaveGreylings/BruteAI.cs
@@ -1,14 +1,145 @@
 ﻿using RagnarsRokare.MobAI;
+using Stateless;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SlaveGreylings
 {
     public class BruteAI : MobAIBase, IControllableMob
     {
+        public MaxStack<Assignment> m_assignment = new MaxStack<Assignment>(20);
+        public MaxStack<Container> m_containers;
+        private float m_searchForNewAssignmentTimer;
+        public float m_foodsearchtimer;
+
+        private class State
+        {
+            public const string Idle = "Idle";
+            public const string Follow = "Follow";
+            public const string Fight = "Fight";
+            public const string Hungry = "Hungry";
+            public const string Assigned = "Assigned";
+            public const string SearchForFood = "SearchForFood";
+            public const string SearchForItems = "SearchForItems";
+            public const string HaveFoodItem = "HaveFoodItem";
+            public const string HaveNoFoodItem = "HaveNoFoodItem";
+        }
+
+        private class Trigger
+        {
+            public const string Update = "Update";
+            public const string TakeDamage = "TakeDamage";
+            public const string Follow = "Follow";
+            public const string Hungry = "Hungry";
+            public const string ItemFound = "ItemFound";
+            public const string ConsumeItem = "ConsumeItem";
+            public const string ItemNotFound = "ItemNotFound";
+
+        }
+
+        readonly StateMachine<string, string>.TriggerWithParameters<(MonsterAI instance, float dt)> UpdateTrigger;
+        readonly StateMachine<string, string>.TriggerWithParameters<IEnumerable<ItemDrop.ItemData>, string, string> LookForItemTrigger;
+
+        public BruteAI() : base()
+        { }
+
+        public BruteAI(MonsterAI instance) : base(instance, State.Idle)
+        {
+            m_containers = new MaxStack<Container>(GreylingsConfig.MaxContainersInMemory.Value);
+            UpdateTrigger = Brain.SetTriggerParameters<(MonsterAI instance, float dt)>(Trigger.Update);
+            LookForItemTrigger = Brain.SetTriggerParameters<IEnumerable<ItemDrop.ItemData>, string, string>(Trigger.ItemFound);
+
+            ConfigureIdle();
+            ConfigureFollow();
+        }
+
+        private void ConfigureIdle()
+        {
+            Brain.Configure(State.Idle.ToString())
+                .PermitIf(Trigger.TakeDamage, State.Fight, () => TimeSinceHurt < 20)
+                .PermitIf(Trigger.Follow, State.Follow, () => (bool)(Instance as MonsterAI).GetFollowTarget())
+                .PermitIf(Trigger.Hungry, State.Hungry, () => (Instance as MonsterAI).Tameable().IsHungry())
+                .PermitIf(UpdateTrigger, State.Assigned, (arg) =>
+                {
+                    if ((m_searchForNewAssignmentTimer += arg.dt) < 2) return false;
+                    m_searchForNewAssignmentTimer = 0f;
+                    return AddNewAssignment(arg.instance.transform.position, m_assignment);
+                })
+                .OnEntry(t =>
+                {
+                    UpdateAiStatus(NView, "Nothing to do, bored");
+                });
+        }
+
+        private void ConfigureFollow()
+        {
+            Brain.Configure(State.Follow)
+                .PermitIf(UpdateTrigger, State.Idle, (args) => !(bool)args.instance.GetFollowTarget())
+                .OnEntry(t =>
+                {
+                    UpdateAiStatus(NView, "Follow");
+                    Invoke<MonsterAI>(Instance, "SetAlerted", false);
+                    m_assignment.Clear();
+                });
+        }
+
+        private void ConfigureIsHungry()
+        {
+            Brain.Configure(State.Hungry)
+                .PermitIf(Trigger.TakeDamage, State.Fight, () => Attacker != null)
+                .PermitIf(Trigger.Follow, State.Follow, () => (bool)(Instance as MonsterAI).GetFollowTarget())
+                .PermitIf(UpdateTrigger, State.SearchForFood, (arg) => (m_foodsearchtimer += arg.dt) > 10)
+                .OnEntry(t =>
+                {
+                    UpdateAiStatus(NView, "Is hungry, no work a do");
+                    m_foodsearchtimer = 0f;
+                });
+
+            Brain.Configure(State.SearchForFood)
+                .SubstateOf(State.Hungry)
+                .Permit(LookForItemTrigger.Trigger, State.SearchForItems)
+                .OnEntry(t =>
+                {
+                    Brain.Fire(LookForItemTrigger, (Instance as MonsterAI).m_consumeItems.Select(i => i.m_itemData), State.HaveFoodItem, State.HaveNoFoodItem);
+                });
+
+            Brain.Configure(State.HaveFoodItem)
+                .SubstateOf(State.Hungry)
+                .Permit(Trigger.ConsumeItem, State.Idle)
+                .OnEntry(t =>
+                {
+                    UpdateAiStatus(NView, "*burps*");
+                    (Instance as MonsterAI).m_onConsumedItem((Instance as MonsterAI).m_consumeItems.FirstOrDefault());
+                    (Instance.GetComponent<Character>() as Humanoid).m_consumeItemEffects.Create(Instance.transform.position, Quaternion.identity);
+                    var animator = Instance.GetType().GetField("m_animator", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(Instance) as ZSyncAnimation;
+                    animator.SetTrigger("consume");
+                    float ConsumeHeal = (Instance as MonsterAI).m_consumeHeal;
+
+                    if (ConsumeHeal > 0f)
+                    {
+                        Instance.GetComponent<Character>().Heal(ConsumeHeal);
+                    }
+                    Brain.Fire(Trigger.ConsumeItem);
+                });
+
+            Brain.Configure(State.HaveNoFoodItem)
+                .SubstateOf(State.Hungry)
+                .PermitIf(Trigger.ItemNotFound, State.Hungry)
+                .OnEntry(t =>
+                {
+                    Brain.Fire(Trigger.ItemNotFound);
+                });
+        }
+
+        private bool AddNewAssignment(Vector3 position, MaxStack<Assignment> m_assignment)
+        {
+            throw new NotImplementedException();
+        }
+
         public override void Follow(Player player)
         {
             NView.InvokeRPC(ZNetView.Everybody, Constants.Z_MobCommand, player.GetZDOID(), "Follow");
