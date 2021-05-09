b30f747c297aae9ddf80e672c1926ffc255e353d
Jan-Eric Fernlöf <jef@clevercode.se>
2021-05-09 21:52:00 +0200

    Register mob when tamed

--- SlaveGreylings/Patches/MonsterAI_patch.cs
+++ SlaveGreylings/Patches/MonsterAI_patch.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using RagnarsRokare.MobAI;
+using System;
+using UnityEngine;
 
 namespace RagnarsRokare.SlaveGreylings
 {
@@ -8,7 +10,7 @@ namespace RagnarsRokare.SlaveGreylings
         [HarmonyPatch(typeof(MonsterAI), "MakeTame")]
         static class MonsterAI_MakeTame_Patch
         {
-            static void Postfix(MonsterAI __instance)
+            static void Postfix(MonsterAI __instance, ZNetView ___m_nview, Character ___m_character)
             {
                 if (MobConfigManager.IsControllableMob(__instance.name))
                 {
@@ -16,6 +18,17 @@ namespace RagnarsRokare.SlaveGreylings
                     __instance.m_consumeItems.Clear();
                     __instance.m_consumeItems.AddRange(mobInfo.PostTameConsumables);
                     __instance.m_consumeSearchRange = 50;
+                    try
+                    {
+                        var uniqueId = ___m_nview.GetZDO().GetString(Constants.Z_CharacterId);
+                        MobManager.RegisterMob(___m_character, uniqueId, mobInfo.AIType, mobInfo.AIConfig);
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Debug.LogError($"Failed to register Mob AI ({mobInfo.AIType}). {e.Message}");
+                        return;
+                    }
+
                 }
             }
         }
