3160e04bee7c59096d2dcb7f6dd662580714b0d6
MrTorstensson <30778800+MrTorstensson@users.noreply.github.com>
2021-06-08 22:29:35 +0200

    Added pickables to Assignment patch.

--- MobAILib/Patches/Assignments_Patch.cs
+++ MobAILib/Patches/Assignments_Patch.cs
@@ -117,5 +117,61 @@ namespace RagnarsRokare.MobAI
                 }
             }
         }
+
+        [HarmonyPatch(typeof(Pickable), "Interact")]
+        static class FPickable_Interact_Patch
+        {
+            public static void Postfix(Pickable __instance)
+            {
+                foreach (MobAIBase mob in MobManager.AliveMobs.Where(m => m.Value.HasInstance()).Where(m => (m.Value.Instance as MonsterAI).GetFollowTarget() == Player.m_localPlayer.gameObject).Select(m => m.Value))
+                {
+                    string interactName = Common.GetPrefabName(__instance.gameObject.name);
+                    if (mob.m_trainedAssignments.Contains(interactName))
+                    {
+                        Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: Already know how to operate the {interactName}!");
+                        return;
+                    }
+                    if (Vector3.Distance(mob.Character.transform.position, __instance.transform.position) < 5 && !mob.m_trainedAssignments.Contains(interactName))
+                    {
+                        if (mob.learningTask == interactName)
+                        {
+                            mob.learningRate += 1;
+                        }
+                        else
+                        {
+                            mob.learningTask = interactName;
+                            mob.learningRate = 0;
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: Introduced to operate the {interactName}.");
+                        }
+                        if (mob.learningRate == 1)
+                        {
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: Greyling is shaking the head when looking at {interactName}.");
+                        }
+                        if (mob.learningRate == 2)
+                        {
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: Increasing the understanding of {interactName}.");
+                        }
+                        if (mob.learningRate == 3)
+                        {
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: Got the basic consept of {interactName}.");
+                        }
+                        if (mob.learningRate == 4)
+                        {
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: There is a shine in the greylings eyes when loocking at the {interactName}.");
+                        }
+                        if (mob.learningRate == 5)
+                        {
+                            mob.m_trainedAssignments.Add(interactName);
+                            mob.NView.GetZDO().Set(Constants.Z_trainedAssignments, mob.m_trainedAssignments.Join());
+                            mob.NView.InvokeRPC(ZNetView.Everybody, Constants.Z_updateTrainedAssignments, mob.UniqueID, mob.m_trainedAssignments.Join());
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: The greyling have now learnt how to operate {interactName}.");
+                            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"{mob.Character.GetHoverName()}: List of known assignments: {mob.m_trainedAssignments.Join()}.");
+                            mob.learningTask = "";
+                            mob.learningRate = 0;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
