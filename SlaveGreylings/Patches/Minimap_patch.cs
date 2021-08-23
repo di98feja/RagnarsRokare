51f94479f00dceb14bf421e0924bf46ab578ea36
Barg <janne.fernlof@hotmail.com>
2021-08-23 21:01:01 +0200

    Error handling

--- SlaveGreylings/Patches/Minimap_patch.cs
+++ SlaveGreylings/Patches/Minimap_patch.cs
@@ -2,6 +2,7 @@
 using RagnarsRokare.MobAI;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace RagnarsRokare.SlaveGreylings
 {
@@ -13,26 +14,34 @@ namespace RagnarsRokare.SlaveGreylings
             private static readonly Dictionary<string, Minimap.PinData> m_mobPins = new Dictionary<string, Minimap.PinData>();
             public static void Postfix()
             {
-                foreach (var mob in MobManager.AliveMobs.Where(m => m.Value.HasInstance()))
+                try
                 {
-                    var pos = mob.Value.Character.transform.position;
-                    var name = mob.Value.NView.GetZDO().GetString(Constants.Z_GivenName);
-                    if (!m_mobPins.ContainsKey(mob.Key))
+                    foreach (var mob in MobManager.AliveMobs.Where(m => m.Value.HasInstance()))
                     {
-                        var pin = Minimap.instance.AddPin(pos, Minimap.PinType.Icon3, name, false, false);
-                        m_mobPins.Add(mob.Key, pin);
+                        var pos = mob.Value.Character.transform.position;
+                        var name = mob.Value.NView.GetZDO().GetString(Constants.Z_GivenName);
+                        if (!m_mobPins.ContainsKey(mob.Key))
+                        {
+                            var pin = Minimap.instance.AddPin(pos, Minimap.PinType.Icon3, name, false, false);
+                            m_mobPins.Add(mob.Key, pin);
+                        }
+                        else
+                        {
+                            m_mobPins[mob.Key].m_pos = pos;
+                            m_mobPins[mob.Key].m_name = name;
+                        }
                     }
-                    else
+                    var idsToRemove = m_mobPins.Where(m => MobManager.AliveMobs.Any(a => (!a.Value.HasInstance()) && (a.Key == m.Key))).Select(m => (m.Key, m.Value)).ToArray();
+                    foreach (var pin in idsToRemove)
                     {
-                        m_mobPins[mob.Key].m_pos = pos;
-                        m_mobPins[mob.Key].m_name = name;
+                        m_mobPins.Remove(pin.Key);
+                        Minimap.instance.RemovePin(pin.Value);
                     }
                 }
-                var idsToRemove = m_mobPins.Where(m => MobManager.AliveMobs.Any(a => (!a.Value.HasInstance()) && (a.Key == m.Key))).Select(m => (m.Key, m.Value)).ToArray();
-                foreach (var pin in idsToRemove)
+                catch (System.Exception e)
                 {
-                    m_mobPins.Remove(pin.Key);
-                    Minimap.instance.RemovePin(pin.Value);
+                    Debug.LogError(e.Message);
+                    throw;
                 }
             }
         }
