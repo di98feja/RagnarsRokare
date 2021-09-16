1ca8e9e03bf929b6e196736030fbe47fed99f52f
MrTorstensson <30778800+MrTorstensson@users.noreply.github.com>
2021-09-16 19:29:31 +0200

    m_consumeHeal and Interactable adjusted.

--- MobAILib/Behaviours/ItemSortingBehaviour.cs
+++ MobAILib/Behaviours/ItemSortingBehaviour.cs
@@ -554,7 +554,7 @@ namespace RagnarsRokare.MobAI
                 {
                     aiBase.StopMoving();
                     Common.Dbgl("Pickable is close", true, "Sorter");
-                    if (m_pickable.Interact((aiBase.Character as Humanoid), false))
+                    if (m_pickable.Interact((aiBase.Character as Humanoid), false,  false))
                     {
                         aiBase.Brain.Fire(Trigger.WaitForPickable);
                         return;
