e8ede95886bff2ad5557dbcbb8df0c70d17aea46
Barg <janne.fernlof@hotmail.com>
2021-09-21 20:25:06 +0200

    PostTameConsumables back in config

--- SlaveGreylings/BruteConfig.cs
+++ SlaveGreylings/BruteConfig.cs
@@ -8,6 +8,7 @@ namespace RagnarsRokare.SlaveGreylings
     {
         public static ConfigEntry<string> BrutePrefabName;
         public static ConfigEntry<string> TamingItemList;
+        public static ConfigEntry<string> HungryItemList;
         public static ConfigEntry<int> PreTameFeedDuration;
         public static ConfigEntry<int> PostTameFeedDuration;
         public static ConfigEntry<int> TamingTime;
@@ -24,13 +25,14 @@ namespace RagnarsRokare.SlaveGreylings
         {
             BrutePrefabName = Config.Bind<string>("General", "Brute_PrefabName", "Greydwarf_Elite", "The prefab to use the Brute ai with (repair structures)");
             TamingItemList = Config.Bind<string>("General", "Brute_TamingItemList", "Dandelion", "Comma separated list if items used to tame Brutes");
+            HungryItemList = Config.Bind<string>("General", "Brute_PostTameConsumables", "QueensJam,Raspberry,Honey,Blueberry,Resin,Dandelions", "Comma separated list if items Brutes eat when hungry");
             PreTameFeedDuration = Config.Bind<int>("General", "Brute_PreTameFeedDuration", 100, "Time before getting hungry after consuming one item during taming");
             PostTameFeedDuration = Config.Bind<int>("General", "Brute_PostTameFeedDuration", 1000, "Time before getting hungry after consuming one item when tame");
             TamingTime = Config.Bind<int>("General", "Brute_TamingTime", 1000, "Total time it takes to tame a Brute");
             TimeLimitOnAssignment = Config.Bind<int>("General", "Brute_TimeLimitOnAssignment", 30, "How long before moving on to next assignment");
             IncludedContainersList = Config.Bind<string>("General", "Brute_IncludedContainersList", "piece_chest_wood", "Comma separated list of container piece names to be searchable by Greylings");
             PreTameConsumables = TamingItemList.Value.Replace(" ", "").Split(',', ';');
-            PostTameConsumables = "Dandelion".Split(',');
+            PostTameConsumables = HungryItemList.Value.Replace(" ", "").Split(',', ';');
             Awareness = Config.Bind<int>("General", "Brute_Awareness", 6, "General awareness, used to calculate search ranges and ability to detect enemies");
             Agressiveness = Config.Bind<int>("General", "Brute_Agressiveness", 8, "Agressivness determines how to behave when fighting and when to give up and flee");
             Mobility = Config.Bind<int>("General", "Brute_Mobility", 10, "Mobility is used to determine how often and how far the mob moves");
